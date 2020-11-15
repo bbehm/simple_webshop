96ce4994d6470b7d848929dcc0976b22673b9085
Bianca Behm <bbehm@student.hive.fi>
2020-11-15 20:48:04 +0200

    added more items and a logo

--- fill_shop.php
+++ fill_shop.php
@@ -8,12 +8,14 @@ if (mysqli_connect_errno()) {
 	exit ();
 }
 
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'treats', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'treats', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'treats', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog collar','accessories', 'dogs', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Cat collar','accessories', 'cats', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'treats', 'Yummy!', 10)");
-mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'treats', 'Yummy!', 10)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog treat','food', 'dogs', 'Yummy for your dog!', 8)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog food','food', 'dogs', 'Help your dog stay strong and healthy!', 10)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Cat food','food', 'cats', 'Your kitty will thank you for this!', 9)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog collar','accessories', 'dogs', 'A cute collar for your best friend!, 15)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Cat collar','accessories', 'cats', 'Your kitten will be happy to wear this!', 13)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Litter box','accessories', 'cats', 'The litter box that will keep both you and your feline friend happy!', 35)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog leash','accessories', 'dogs', 'Flex leash for happy walks!', 19)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Dog squeeky toy','accessories', 'dogs', 'THE toy for your dog!', 6)");
+mysqli_query($database, "INSERT INTO rush00.items (itemName, category, subcategory, itemDescription, price) VALUES ('Cat toy','accessories', 'cats', 'This will keep you kitty entertained!', 5)");
 
 ?>
